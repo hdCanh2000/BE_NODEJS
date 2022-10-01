e2a2aa10a0c54ed9ad39c97f043b1f48f0e52a94
unknown <chutrunghieu12@gmail.com>
2022-10-01 11:40:21 +0700

    M

--- config/database.js
+++ config/database.js
@@ -6,9 +6,6 @@ dotenv.config();
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
 db.sync();
