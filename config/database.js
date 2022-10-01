4c7cda6474db45f7abe49c7c5265ae75827485d3
unknown <chutrunghieu12@gmail.com>
2022-10-01 11:00:33 +0700

    m

--- config/database.js
+++ config/database.js
@@ -3,11 +3,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  });
+const db = new Sequelize(process.env.DATABASE_URL);
 
-// db.sync();
+db.sync();
 
 module.exports = db;
