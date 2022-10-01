5ec06bf63d9656c67a026814afc050fcfbeaddc6
unknown <chutrunghieu12@gmail.com>
2022-10-01 10:28:45 +0700

    m

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
 
-const db = new Sequelize('dwt', 'postgres', 'huonghuong1412', {
-    host: 'localhost',
-    dialect: 'postgres',
+dotenv.config();
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
   });
 
 // db.sync();
