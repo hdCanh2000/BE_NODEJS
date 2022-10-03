5a7052d72eae5f7453b1aa522e223984837d2ca0
huonghuong1412 <62413703+huonghuong1412@users.noreply.github.com>
2022-10-03 08:34:42 +0700

    crud worktrack log

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const positionLevelRouter = require('./api/positionLevel/positionLevel.routes');
 const unitRoutes = require('./api/unit/unit.routes');
 const missionRouters = require('./api/misson/mission.routes');
 const worktrackRoutes = require('./api/worktrack/worktrack.routes');
+const worktrackLogRoutes = require('./api/worktrackLog/worktrackLog.routes');
 const kpiNormRoutes = require('./api/kpiNorm/kpiNorm.routes');
 
 const router = express.Router();
@@ -17,6 +18,7 @@ router.use('/positionLevels', positionLevelRouter);
 router.use('/unit', unitRoutes);
 router.use('/mission', missionRouters);
 router.use('/worktracks', worktrackRoutes);
+router.use('/worktrackLogs', worktrackLogRoutes);
 router.use('/kpiNorms', kpiNormRoutes);
 
 module.exports = router;
