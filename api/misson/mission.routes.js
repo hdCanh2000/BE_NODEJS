619617d104d318c25d2d425a5f1bd1bb07c48081
unknown <chutrunghieu12@gmail.com>
2022-10-05 09:17:25 +0700

    m

--- api/misson/mission.routes.js
+++ api/misson/mission.routes.js
@@ -4,7 +4,7 @@ const missionController = require('./mission.controllers');
 
 const router = express.Router();
 
-router.post('/', auth('admin'), missionController.addMission);
+router.post('/', auth(['admin', 'manager']), missionController.addMission);
 router.get('/', auth(['admin', 'manager']), missionController.getMission);
 router.get('/:id', auth(['admin', 'manager']), missionController.getDetailMission);
 router.put('/:id', auth(['admin', 'manager']), missionController.updateMission);
