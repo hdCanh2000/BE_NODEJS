821e296937f23c6730012e977fe91ec1099578dd
unknown <chutrunghieu12@gmail.com>
2022-10-13 10:53:53 +0700

    fix-worktrack

--- api/worktrack/worktrack.routes.js
+++ api/worktrack/worktrack.routes.js
@@ -8,7 +8,7 @@ router.get('/', auth(['admin', 'manager', 'user']), worktrackController.getAll);
 router.get('/:id', auth(['admin', 'manager', 'user']), worktrackController.getById);
 router.get('/user/:user_id', auth(['admin', 'manager', 'user']), worktrackController.getAllByUserId);
 router.post('/', auth(['admin', 'manager', 'user']), worktrackController.addKpiNormForUser);
-router.put('/:id', auth(['admin', 'manager', 'user']), worktrackController.updateById);
+router.put('/:id', auth(['admin', 'manager', 'user']), worktrackController.updateWorkTrackById);
 router.delete('/:id', auth(['admin', 'manager', 'user']), worktrackController.deleteById);
 
 module.exports = router;
