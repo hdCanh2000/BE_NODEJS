5827a6aa55a5f1c542d1cc575ce3817cb43d0c43
huonghuong1412 <62413703+huonghuong1412@users.noreply.github.com>
2022-10-17 14:23:36 +0700

    fix user

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcrypt');
+const { Op } = require('sequelize');
+const sequelize = require('sequelize');
 const model = require('../../models/index');
 const ApiError = require('../../utils/ApiError');
 
@@ -72,4 +74,61 @@ exports.deleteById = async (id) => {
     }
     await resource.destroy();
     return resource;
+};
+
+exports.findAll = async ({ userId, query }) => {
+    const { page, limit, text } = query;
+    let searchValue = '';
+    if (text) searchValue = text.toLowerCase();
+    try {
+        const total = await model.users.count();
+        const getUserById = await model.users.findOne({ where: { id: userId, isDelete: false } });
+        if (getUserById.role === 'admin') {
+            const data = await model.users.findAll({
+                offset: (page - 1) * limit || 0,
+                limit,
+                order: [
+                    ['id', 'DESC'],
+                ],
+                include: [
+                    {
+                        model: model.departments,
+                    },
+                    {
+                        model: model.positions,
+                    },
+                ],
+                where: {
+                    isDelete: false,
+                    [Op.or]: [
+                        sequelize.where(sequelize.fn('LOWER', sequelize.col('users.name')), 'LIKE', `%${searchValue}%`),
+                    ],
+                },
+            });
+           return { data, pagination: { page: parseInt(page), limit: parseInt(limit), totalRows: data.length, total } };
+        }
+        if (getUserById.role === 'manager') {
+            const data = await model.users.findAll({
+                include: [
+                    {
+                        model: model.departments,
+                    },
+                    {
+                        model: model.positions,
+                    },
+                ],
+                where: {
+                    department_id: getUserById.department_id,
+                    isDelete: false,
+                    [Op.or]: [
+                        sequelize.where(sequelize.fn('LOWER', sequelize.col('users.name')), 'LIKE', `%${searchValue}%`),
+                    ],
+                },
+            });
+           return { data, pagination: { page: parseInt(page), limit: parseInt(limit), totalRows: data.length, total } };
+        }
+        return null;
+    } catch (error) {
+        return error;
+    }
 };
