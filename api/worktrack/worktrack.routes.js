0e1fe82e15ca940b519550c3a727e1533616e0df
unknown <chutrunghieu12@gmail.com>
2022-10-20 13:50:30 +0700

    add-status

--- api/worktrack/worktrack.routes.js
+++ api/worktrack/worktrack.routes.js
@@ -11,6 +11,6 @@ router.get('/:id', auth(['admin', 'manager', 'user']), worktrackController.getBy
 router.post('/', auth(['admin', 'manager', 'user']), worktrackController.addKpiNormForUser);
 router.put('/:id', auth(['admin', 'manager', 'user']), worktrackController.updateWorkTrackById);
 router.delete('/:id', auth(['admin', 'manager', 'user']), worktrackController.deleteById);
-router.get('/workTrackPending', auth(['admin', 'manager', 'user']), worktrackController.getWorkTrackWithPending);
+router.get('/workTrackPending', auth(['admin', 'manager', 'user']), worktrackController.getWorkTrackByStatus);
 
 module.exports = router;
