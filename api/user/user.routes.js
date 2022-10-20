23bb1ffdc6962529428b89730ad15ca5c2e2f194
unknown <chutrunghieu12@gmail.com>
2022-10-20 16:58:07 +0700

    M

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -7,10 +7,10 @@ const router = express.Router();
 router.post('/', auth('admin'), userController.addUser);
 router.get('/', auth(['admin', 'user', 'manager']), userController.getAllUser);
 router.get('/exportExcel', auth(['admin', 'manager']), userController.exportExcel);
+router.put('/changePassword', auth(['admin', 'user', 'manager']), userController.changePassword);
 router.get('/:id', auth(['admin', 'user', 'manager']), userController.getUserDetail);
 router.put('/:id', auth(['admin', 'user', 'manager']), userController.updateProfile);
 router.get('/department/:department_id', auth(['admin', 'user', 'manager']), userController.getAllUserByDepartmentId);
-router.put('/changePassword', auth(['admin', 'user', 'manager']), userController.changePassword);
 // router.delete('/', auth('admin'), userController.deleteUser);
 
 module.exports = router;
