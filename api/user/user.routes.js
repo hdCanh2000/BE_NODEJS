b3b7350b6d6440676c4b893d08c0c9a9fbe0a749
Anh Tuan <tuanva9420@gmail.com>
2022-10-20 13:55:50 +0700

    editExport

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -6,11 +6,11 @@ const router = express.Router();
 
 router.post('/', auth('admin'), userController.addUser);
 router.get('/', auth(['admin', 'user', 'manager']), userController.getAllUser);
+router.get('/exportExcel', auth(['admin', 'manager']), userController.exportExcel);
 router.get('/:id', auth(['admin', 'user', 'manager']), userController.getUserDetail);
 router.put('/:id', auth(['admin', 'user', 'manager']), userController.updateProfile);
 router.get('/department/:department_id', auth(['admin', 'user', 'manager']), userController.getAllUserByDepartmentId);
 router.put('/changePassword', auth(['admin', 'user', 'manager']), userController.changePassword);
 // router.delete('/', auth('admin'), userController.deleteUser);
-router.post('/exportExcel', auth(['admin', 'manager']), userController.exportExcel);
 
 module.exports = router;
