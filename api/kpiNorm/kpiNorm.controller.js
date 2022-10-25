14e8c27a7ae3cec2b78dd3d1cd47d1db29168058
TrinhDat <cv.trinhvietdat1805@gmail.com>
2022-10-25 13:25:34 +0700

    export Excel

--- api/kpiNorm/kpiNorm.controller.js
+++ api/kpiNorm/kpiNorm.controller.js
@@ -1,5 +1,6 @@
+const ExcelJs = require('exceljs');
 const kpiNormService = require('./kpiNorm.service');
-const ApiError = require('../../utils/ApiError');
+const model = require('../../models/index');
 
 exports.addKpiNorm = async (req, res) => {
     try {
@@ -54,6 +55,58 @@ exports.deleteKpiNorm = async (req, res) => {
     try {
         const deleteKpi = await kpiNormService.deleteById(id);
         return res.status(200).json({ message: 'Success!', data: deleteKpi });
+    } catch (error) {
+        return res.status(404).json({ message: 'Error!', error: error.message });
+    }
+};
+exports.exportExcel = async (req, res) => {
+    try {
+        const kpiNorms = await model.kpiNorms.findAll();
+        const position = await model.positions.findAll();
+        const showPosition = (id) => {
+            const newData = position.filter((item) => item.id === id);
+            return newData[0]?.name;
+        };
+        const showParent = (id) => {
+            const newData = position.filter((item) => item.id === id);
+            return newData[0]?.name;
+        };
+        const kpiNorm = kpiNorms.filter((items) => items.dataValues.role !== 'admin').map((item) => (
+            {
+                name: item.dataValues.name,
+                quantity: item.dataValues.quantity,
+                taskType: item.dataValues.taskType,
+                position: showPosition(item.dataValues.position_id),
+                parent: showParent(item.dataValues.parent_id),
+                description: item.dataValues.description,
+                kpi_value: item.dataValues.kpi_value,
+                descriptionKpiValue: item.dataValues.descriptionKpiValue,
+            }));
+        const workbook = new ExcelJs.Workbook();
+        const worksheet = workbook.addWorksheet('kpiNorm');
+        worksheet.columns = [
+            { header: 'STT', key: 'stt', width: 7 },
+            { header: 'Tên nhiệm vụ', key: 'name', width: 70 },
+            { header: 'Số lượng', key: 'quantity', width: 10 },
+            { header: 'Loại nhiệm vụ', key: 'taskType', width: 25 },
+            { header: 'Vị trí đảm nhiệm', key: 'position', width: 30 },
+            { header: 'Thuộc nhiệm vụ cha', key: 'parent', width: 30 },
+            { header: 'Thang điểm', key: 'kpi_value', width: 20 },
+            { header: 'Tương đương với kết quả', key: 'descriptionKpiValue', width: 50 },
+            { header: 'Mô tả /Diễn giải', key: 'description', width: 25 },
+        ];
+        let count = 1;
+        kpiNorm.forEach((e) => {
+            e.stt = count;
+            worksheet.addRow(e);
+            count += 1;
+        });
+        worksheet.getRow(1).eachCell((cell) => {
+            // eslint-disable-next-line no-param-reassign
+            cell.font = { bold: true, size: 12 };
+        });
+        await workbook.xlsx.writeFile('Danh sách nhiệm vụ.xlsx');
+        res.download('Danh sách nhiệm vụ.xlsx');
     } catch (error) {
         return res.status(404).json({ message: 'Error!', error: error.message });
     }
