f1f111f3f5f3d9d00b0ab8e8d6e3c64c1438e66f
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-01-08 22:08:55 +0700

    new upload sample

--- api/target/target.controller.js
+++ api/target/target.controller.js
@@ -2,7 +2,14 @@ const ApiError = require('../../utils/ApiError')
 const targetService = require('./target.service')
 const uploadFile = require('../../middleware/upload')
 const dailyWorkServices = require('../dailyWork/dailyWork.service')
-
+const ExcelJs = require('exceljs')
+const moment = require('moment')
+const userServices = require('../user/user.service')
+const departmentService = require('../department/department.service')
+const fetch = require('node-fetch')
+const { readFileSync } = require('fs')
+const fs = require('fs')
+const FormData = require('form-data')
 exports.getTarget = async (req, res) => {
   const { status, departmentId, userId } = req.query
   try {
@@ -154,10 +161,155 @@ exports.updateTarget = async (req, res) => {
 }
 
 exports.exportTarget = async (req, res) => {
-  const targets = await targetService.searchTargets(req.query)
-  const dailyWorks = await dailyWorkServices.searchDailyWorks(req.query)
-  return res.status(200).send({
-    targets,
-    dailyWorks,
-  })
+  try {
+    if (!req.query.start || !req.query.end) {
+      return res.status(400).send(new ApiError('Missing start and end date', 400))
+    }
+    const targets = await targetService.searchTargets(req.query)
+    const dailyWorks = await dailyWorkServices.searchDailyWorks(req.query)
+    //get user
+    let userName = ''
+    let position = ''
+    if (req.query.userId) {
+      const user = await userServices.findUser(req.query.userId)
+      userName = user.name
+      position = user.position.name
+    }
+    if (req.query.departmentId) {
+      const dpm = await departmentService.detailDepartment(req.query.departmentId)
+      position = dpm.name
+    }
+
+    const workbook = new ExcelJs.Workbook()
+    const sample = await workbook.xlsx.readFile('./resources/static/files/DWT_export_sample.xlsx')
+    const sheet = sample.getWorksheet(2)
+    //insert calendar base on start date and end date
+    const start = moment(req.query.start, 'MM-DD-YYYY')
+    const month = start.month()
+    //fill user, position and month to worksheet
+    sheet.getCell('D2').value = 'Vị trí: ' + position
+    sheet.getCell('AI1').value = userName
+    sheet.getCell('AI2').value = month + 1
+    const calendarStartCol = 10
+    for (let i = 0; i < start.daysInMonth(); i++) {
+      const currentCalenderDay = moment(`${month + 1}-${i + 1}-${start.year()}`, 'MM-D-YYYY').locale('vi')
+      const calenderText = currentCalenderDay.format('D dd')
+
+      const calenderCell = sheet.getRow(5).getCell(i + calendarStartCol)
+      // set background color for weekend
+      if (currentCalenderDay.day() === 0) {
+        calenderCell.style = {
+          ...calenderCell.style,
+          fill: { ...calenderCell.fill, fgColor: { argb: '00ff9f9f' } },
+        }
+      }
+      if (currentCalenderDay.day() === 6) {
+        calenderCell.style = {
+          ...calenderCell.style,
+          fill: { ...calenderCell.fill, fgColor: { argb: '00fff2cc' } },
+        }
+      }
+      calenderCell.value = calenderText
+    }
+    //start insert target data from 6th row
+    const targetsStartInsertRow = 6
+    //reverse target to insert from bottom to top
+    const reversedTargets = targets.reverse()
+    let currentRowCursor = targetsStartInsertRow
+    let totalManDayEstimate = 0
+    reversedTargets.forEach((target, index) => {
+      //insert then fill then insert at same position => fill data reversely
+      const insertedRow = sheet.insertRow(targetsStartInsertRow, null, 'i+')
+      const row = insertedRow.number
+      fillCell(sheet, `A${row}`, reversedTargets.length - index, 'ffffffff')
+      fillCell(sheet, `B${row}`, target.name ?? '', 'ffffffff')
+      fillCell(sheet, `C${row}`, '-', 'ffffffff')
+      fillCell(sheet, `D${row}`, target.deadline ? moment(target.deadline).format('DD/MM/YYYY') : '', 'ffffffff')
+      fillCell(sheet, `E${row}`, target.quantity ?? '', 'ffffffff')
+      fillCell(sheet, `F${row}`, target?.unit?.name ?? '', 'ffffffff')
+      fillCell(sheet, `G${row}`, target.manDay ?? '', 'ffffffff')
+      // calculate manday estimate
+      let manDayEstimate = 0
+      let totalCompletedTasks = 0
+      target.TargetLogs.forEach(log => {
+        if (log.status === 'completed') {
+          totalCompletedTasks += log.quantity
+        }
+      })
+      manDayEstimate = totalCompletedTasks * (target.manDay ?? 1)
+      totalManDayEstimate += manDayEstimate
+      fillCell(sheet, `H${row}`, manDayEstimate, 'ffffffff')
+      fillCell(sheet, `I${row}`, '-', 'ffffffff')
+      //ínsert target log into current row
+      const targetLogs = target.TargetLogs
+      targetLogs.forEach((log, index) => {
+        const logDay = log.reportDate ? moment(log.reportDate, 'YYYY-MM-DD').date() : moment(log.noticedDate, 'YYYY-MM-DD').date()
+        const logDayCalendarCell = sheet.getRow(row).getCell(logDay + calendarStartCol - 1)
+        const completedColor = '0005a34a'
+        const workingColor = '006571ff'
+        const isCompleted = log.status === 'completed'
+        const isWarning = !!log.noticedStatus
+        if (isCompleted) {
+          logDayCalendarCell.style = {
+            ...logDayCalendarCell.style,
+            fill: { ...logDayCalendarCell.fill, fgColor: { argb: completedColor } },
+          }
+          logDayCalendarCell.value = log.quantity
+        } else if (isWarning && !isCompleted) {
+          logDayCalendarCell.style = {
+            ...logDayCalendarCell.style,
+            fill: { ...logDayCalendarCell.fill, fgColor: { argb: workingColor } },
+          }
+          logDayCalendarCell.value = `! ${log.quantity ?? 0}`
+        } else {
+          logDayCalendarCell.value = log.quantity
+        }
+      })
+      currentRowCursor++
+    })
+    // insert total estimated manday
+    fillCell(sheet, `H${currentRowCursor}`, totalManDayEstimate, '00e2f0d9')
+    const timeStamp = new Date().getTime()
+    const fileName = './resources/static/files/' + `${timeStamp}_dwt_thang_${month + 1}.xlsx`
+    await workbook.xlsx.writeFile(fileName)
+    //chmod file before send to avoid htaccess
+    /*
+    * The number 777 in octal notation is the number 511 in decimal notation.
+    * fs.chmod(path, 0777) and fs.chmod(path, 511) do the same thing,
+    * but fs.chmod(path, 777) does not.
+    * https://stackoverflow.com/questions/20769023/using-nodejs-chmod-777-and-0777
+    * */
+    fs.chmodSync(fileName, 0o777)
+    const uploadUrl = 'https://report.sweetsica.com/api/report/upload'
+    const formData = new FormData()
+    formData.append('files', fs.createReadStream(fileName))
+    formData.append('userId', req.query.userId)
+    const uploadRes = await fetch(uploadUrl, {
+      method: 'POST',
+      body: formData,
+    })
+    if (!uploadRes.ok) {
+      throw new Error('Upload file failed')
+    }
+    const uploadResJson = await uploadRes.json()
+    //delete file after upload
+    await fs.unlinkSync(fileName)
+    return res.status(200).send(uploadResJson)
+  } catch (err) {
+    console.log(err)
+    res.status(500).send({
+      message: `Internal server error: ${err}`,
+    })
+  }
+}
+
+function fillCell(sheet, cellIndex, value, color) {
+  const cell = sheet.getCell(cellIndex)
+  cell.value = value
+  //set color
+  cell.style = {
+    ...cell.style,
+    fill: { ...cell.fill, fgColor: { argb: color } },
+    font: { ...cell.font, bold: false },
+  }
 }
