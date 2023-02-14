831a35eb945daa8b8c693cbe2682b278c2cf8b4d
CongPp1 <103237012+CongPp1@users.noreply.github.com>
2023-02-14 16:54:57 +0700

    AddRecordController

--- api/keyRecord/keyRecord.controller.js
+++ api/keyRecord/keyRecord.controller.js
@@ -11,6 +11,17 @@ const getAllByReportId = async(req, res) => {
     }
 }
 
+const addRecord = async(req, res) => {
+    try {
+        const result = await keyRecordServices.addNewRecord(req.body);
+        res.status(200).json({message: 'Successfully!', result});
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({message: 'ERROR!', error});
+    }
+}
+
 module.exports = {
-    getAllByReportId
+    getAllByReportId,
+    addRecord
 }
