fb6ffdd3c6f78f0cb76846d29d5dd8a9ce674fd3
CongPp1 <103237012+CongPp1@users.noreply.github.com>
2023-02-14 16:52:40 +0700

    AddNewRoute

--- api/keyRecord/keyRecord.routes.js
+++ api/keyRecord/keyRecord.routes.js
@@ -4,5 +4,6 @@ const keyRecordController = require('./keyRecord.controller');
 const router = express.Router();
 
 router.get('/:keyReportId', auth(['admin', 'manager', 'user']), keyRecordController.getAllByReportId);
+router.post('/', auth(['admin', 'manager', 'user']), keyRecordController.addRecord);
 
 module.exports = router;
