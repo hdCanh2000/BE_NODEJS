a5ad1241af7b6466aca470888a21662ac80c5a0f
CongPp1 <103237012+CongPp1@users.noreply.github.com>
2023-02-22 17:55:54 +0700

    EditConstraint

--- models/targetLog.model.js
+++ models/targetLog.model.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       TargetLog.belongsTo(models.TargetInfos, {foreignKey: 'targetInfoId', targetKey: 'id'});
+      TargetLog.hasMany(models.keyRecord, {foreignKey: 'targetLogId', targetKey: 'id', onDelete: 'SET NULL', onUpdate: 'CASCADE'});
     }
   }
 
